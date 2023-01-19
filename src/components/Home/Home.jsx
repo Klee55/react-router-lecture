5a736b792cb44c0311566e9ea1af247cab4d90fc
Khue Lee <khuelee55@gmail.com>
2023-01-18 18:40:39 -0600

    updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Home() {
+
+  const history = useHistory();
+
+  const routeToAnimal = () => {
+    alert('animals');
+    history.push('/animals');
+  }
+
   return (
     <div>
       <h1>HOME</h1>
       <img src="https://pixy.org/download/4718707/" width={500}/>
+      <button onClick={routeToAnimal}>go find animals</button>
     </div>
   );
 }
